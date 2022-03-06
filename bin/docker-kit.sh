de8033affbbf5013f8ee77f47b30db127395f8c5
Marc Partensky <marc.partensky@gmail.com>
2022-03-06 01:48:06 +0100

    less indent

--- bin/docker-kit.sh
+++ bin/docker-kit.sh
@@ -5,29 +5,43 @@ export COMPOSE_PATH_SEPARATOR=":"
 export COMPOSE_FILE=$(find $PROJECT_PATH/services/*.yml | xargs -I @ echo -n @:)
 export COMPOSE_FILE=${COMPOSE_FILE::-1}
 
+
 if [ "$1" = "-f" ]
 then
     docker-compose --file $@
-else
-    if [ -f $PWD/docker-compose.yml ]
+    exit
+fi
+
+if [ -f $PWD/docker-compose.yml ]
+then
+    if [ "$#" -ne 1 ]
     then
-        docker-compose --file $PWD/docker-compose.yml $@
-    else
-        docker-compose $@
-        if [ "$1" = "up" ]
-        then
-            if [ "$2" = "-d" ]
-            then
-                docker-compose logs -f ${@: 3}
-            else
-                docker-compose logs -f ${@: 2}
-            fi
-        elif [ "$1" = "start" ]
-        then
-            docker-compose logs -f ${@: 2}
-        elif [ "$1" = "restart" ]
-        then
-            docker-compose logs -f ${@: 2}
-        fi
+        docker-compose ps
+        exit
     fi
+    docker-compose --file $PWD/docker-compose.yml $@
+    exit
+fi
+
+if [ "$#" -ne 1 ]
+then
+    docker-compose ps
+    exit
+fi
+
+docker-compose $@
+if [ "$1" = "up" ]
+then
+    if [ "$2" = "-d" ]
+    then
+        docker-compose logs -f ${@: 3}
+    else
+        docker-compose logs -f ${@: 2}
+    fi
+elif [ "$1" = "start" ]
+then
+    docker-compose logs -f ${@: 2}
+elif [ "$1" = "restart" ]
+then
+    docker-compose logs -f ${@: 2}
 fi
