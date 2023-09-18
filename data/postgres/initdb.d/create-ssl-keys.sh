a73471643eb89bce741ca5b8b1d89725b2701ae2
Marc Partensky <marc.partensky@gmail.com>
2023-09-18 13:49:47 +0200

    create-ssl-keys.sh

--- data/postgres/initdb.d/create-ssl-keys.sh
+++ data/postgres/initdb.d/create-ssl-keys.sh
@@ -5,4 +5,5 @@ openssl req -new -text -passout pass:abcd -subj /CN=localhost -out server.req -k
 openssl rsa -in privkey.pem -passin pass:abcd -out server.key
 openssl req -x509 -in server.req -text -key server.key -out server.crt
 chmod 600 server.key
+chown postgres:postgres server.key
 # test $(uname -s) = Linux && chown 70 server.key
