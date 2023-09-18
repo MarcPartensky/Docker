9bc22acb3f0d30e73bb141e970e55785557e4ae1
Marc Partensky <marc.partensky@gmail.com>
2023-09-18 13:37:02 +0200

    postgres entrypoints

---
+++ data/postgres/initdb.d/create-ssl-keys.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd /var/lib/postgresql/certs
+openssl req -new -text -passout pass:abcd -subj /CN=localhost -out server.req -keyout privkey.pem
+openssl rsa -in privkey.pem -passin pass:abcd -out server.key
+openssl req -x509 -in server.req -text -key server.key -out server.crt
+chmod 600 server.key
+test $(uname -s) = Linux && chown 70 server.key
