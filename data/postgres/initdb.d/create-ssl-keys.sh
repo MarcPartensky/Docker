b0115be27dde9c7266e97cfc0bd998ecaecf4be9
Marc Partensky <marc.partensky@gmail.com>
2023-09-18 14:21:24 +0200

    dont check validity

--- data/postgres/initdb.d/create-ssl-keys.sh
+++ data/postgres/initdb.d/create-ssl-keys.sh
@@ -8,8 +8,6 @@ openssl req -new -text -newkey rsa:8192 -passout pass:$POSTGRES_PASSWORD -subj /
 openssl rsa -in privkey.pem -passin pass:$POSTGRES_PASSWORD -out server.key
 # derived certificate
 openssl req -x509 -in server.req -text -key server.key -out server.crt
-# check if the certificate validity
-openssl req -in server.req -text -verify -noou
 
 # change permissions to the root certificate for postgres
 chmod 600 server.key
