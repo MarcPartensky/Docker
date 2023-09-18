c0dfd22ddb260be1a2a8342e0b6c57468f0581b8
Marc Partensky <marc.partensky@gmail.com>
2023-09-18 14:15:46 +0200

    postgres new openssl script

--- data/postgres/initdb.d/create-ssl-keys.sh
+++ data/postgres/initdb.d/create-ssl-keys.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
 cd /var/lib/postgresql/certs
-openssl req -new -text -passout pass:abcd -subj /CN=localhost -out server.req -keyout privkey.pem
-openssl rsa -in privkey.pem -passin pass:abcd -out server.key
+
+# private key and certificate from it
+openssl req -new -text -newkey rsa:8192 -passout pass:$POSTGRES_PASSWORD -subj /CN=localhost -out server.req -keyout privkey.pem
+# self signed root certificate
+openssl rsa -in privkey.pem -passin pass:$POSTGRES_PASSWORD -out server.key
+# derived certificate
 openssl req -x509 -in server.req -text -key server.key -out server.crt
+# check if the certificate validity
+openssl req -in server.req -text -verify -noou
+
+# change permissions to the root certificate for postgres
 chmod 600 server.key
 chown postgres:postgres server.key
-# test $(uname -s) = Linux && chown 70 server.key
